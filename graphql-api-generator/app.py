9993328ca6847615cc0e77a635bf0f11873aaf26
Robin Keskisärkkä <robin.keskisarkka@liu.se>
2019-05-15 14:38:04 +0200

    return schema from app

--- graphql-api-generator/app.py
+++ graphql-api-generator/app.py
@@ -1,26 +1,62 @@
 #!/usr/bin/env python3
+from graphql import GraphQLSchema
 
 from generator import schema_extender
 import configparser
 
-def add_id_to_type(input_file, output_file):
-    copy_file(input_file, output_file)
+def add_id_to_type(input):
+    if type(input) == str:
+        schema = schema_extender.read_schema_file(input)
+    elif type(input) == GraphQLSchema:
+        schema = input
+    else:
+        raise ValueError('Expected input to be a schema or input file')
+
+    return schema
 
 
-def add_query_by_id(input_file, output_file):
-    copy_file(input_file, output_file)
+def add_query_by_id(input):
+    if type(input) == str:
+        schema = schema_extender.read_schema_file(input)
+    elif type(input) == GraphQLSchema:
+        schema = input
+    else:
+        raise ValueError('Expected input to be a schema or input file')
+
+    return schema
 
 
-def add_query_by_type(input_file, output_file):
-    copy_file(input_file, output_file)
+def add_query_by_type(input):
+    if type(input) == str:
+        schema = schema_extender.read_schema_file(input)
+    elif type(input) == GraphQLSchema:
+        schema = input
+    else:
+        raise ValueError('Expected input to be a schema or input file')
+
+    return schema
 
 
-def add_input_types_to_create_objects(input_file, output_file):
-    copy_file(input_file, output_file)
+def add_input_types_to_create_objects(input):
+    if type(input) == str:
+        schema = schema_extender.read_schema_file(input)
+    elif type(input) == GraphQLSchema:
+        schema = input
+    else:
+        raise ValueError('Expected input to be a schema or input file')
+
+    return schema
 
 
-def add_mutation_for_creating_objects(input_file, output_file):
-    copy_file(input_file, output_file)
+def add_mutation_for_creating_objects(input):
+    if type(input) == str:
+        schema = schema_extender.read_schema_file(input)
+    elif type(input) == GraphQLSchema:
+        schema = input
+    else:
+        raise ValueError('Expected input to be a schema or input file')
+
+    return schema
 
 
 def copy_file(input_file, output_file):
--- graphql-api-generator/generator/schema_extender.py
+++ graphql-api-generator/generator/schema_extender.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
 import graphql
-from graphql import build_schema, extend_schema, parse, print_schema
-from graphql import GraphQLObjectType, GraphQLInputObjectType, GraphQLInputField, GraphQLSchema, GraphQLString
+from graphql import build_schema, extend_schema, parse, print_schema, \
+    GraphQLObjectType, GraphQLInputObjectType, GraphQLInputField, GraphQLSchema, GraphQLString, introspection_types, \
+    is_object_type, GraphQLField
+
 from graphql.type import is_input_type
 
 
@@ -10,3 +12,17 @@ def read_schema_file(filename):
     with open(filename, 'r', encoding='utf8') as s_file:
         schema = build_schema(''.join(s_file))
     return schema
+
+
+def add_id_to_type(schema):
+    for (n, t) in get_object_types(schema):
+        pass
+        #f = GraphQLField(ftype)
+        #t.fields = GraphQLObjectType(name=n, fields=f)
+        #in_obj = GraphQLObjectType(name=t, fields=i_fields)
+
+    return schema
+
+
+def get_object_types(schema):
+    return [(n, t) for n, t in schema.type_map.items() if n not in introspection_types and is_object_type(t)]

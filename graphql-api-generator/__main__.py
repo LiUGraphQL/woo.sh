c79110db6217e3ff492fe3d6235b2ca0b44c3aa4
Robin Keskisärkkä <robin.keskisarkka@liu.se>
2019-05-15 10:34:00 +0200

    add requirements.txt and config file

--- graphql-api-generator/__main__.py
+++ graphql-api-generator/__main__.py
@@ -1,7 +1,30 @@
 #!/usr/bin/env python3
+import configparser
+
+# Should we:
+#   make config global/static for all other classes
+#   provide config as arg where necessary
+#   or have all modules load it themselves
 
 def run():
-    print("test")
+    config = load_config()
+    print_config(config)
+
+def load_config():
+    config = configparser.ConfigParser()
+    config.read_file(open("resources/config.cfg"))
+    return config
+
+def print_config(config):
+    for section in config.sections():
+        print("Section: {0}".format(section));
+        for arg in config[section]:
+            print("   {0} = {1}".format(arg, config[section][arg]))
+
+
+
+
+
 
 if __name__ == '__main__':
     run()

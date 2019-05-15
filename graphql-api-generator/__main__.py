47383bf0c94645efb7ed1e1c8666c013bdf3c2a7
Robin Keskisärkkä <robin.keskisarkka@liu.se>
2019-05-15 14:36:04 +0200

    app returns schema

--- graphql-api-generator/__main__.py
+++ graphql-api-generator/__main__.py
@@ -2,7 +2,6 @@
 
 import app
 
-
 if __name__ == '__main__':
     config = app.load_config()
-    app.copy_file("resources/example-schema.graphql", "resources/dummy.txt")
+    #app.copy_file('resources/example-schema.graphql', 'resources/dummy.txt')
--- graphql-api-generator/generator/schema_extender.py
+++ graphql-api-generator/generator/schema_extender.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+import graphql
+from graphql import build_schema, extend_schema, parse, print_schema
+from graphql import GraphQLObjectType, GraphQLInputObjectType, GraphQLInputField, GraphQLSchema, GraphQLString
+from graphql.type import is_input_type
+
+
+def read_schema_file(filename):
+    with open(filename, 'r', encoding='utf8') as s_file:
+        schema = build_schema(''.join(s_file))
+    return schema

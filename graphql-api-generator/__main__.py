2721519985793ee13e40eba72180ef9a6a89821d
Robin Keskisärkkä <robin.keskisarkka@liu.se>
2019-05-17 16:37:56 +0200

    add support for generating query by id

--- graphql-api-generator/__main__.py
+++ graphql-api-generator/__main__.py
@@ -4,4 +4,7 @@ import app
 
 if __name__ == '__main__':
     config = app.load_config()
-    schema = app.add_id_to_type('resources/example-schema.graphql')
+    schema = app.to_schema('resources/example-schema.graphql')
+    schema = app.add_id_to_type(schema)
+    schema = app.add_query_by_id(schema)
+    app.pprint(schema)
--- graphql-api-generator/app.py
+++ graphql-api-generator/app.py
@@ -4,68 +4,43 @@ from graphql import GraphQLSchema, print_schema
 from generator import schema_extender
 import configparser
 
-def add_id_to_type(input):
-    if type(input) == str:
-        schema = schema_extender.read_schema_file(input)
-    elif type(input) == GraphQLSchema:
-        schema = input
-    else:
-        raise ValueError('Expected input to be a schema or input file')
 
+def add_id_to_type(data):
+    schema = to_schema(data)
     schema = schema_extender.add_id_to_type(schema)
-    #print(print_schema(schema))
-
     return schema
 
 
-def add_query_by_id(input):
-    if type(input) == str:
-        schema = schema_extender.read_schema_file(input)
-    elif type(input) == GraphQLSchema:
-        schema = input
+def add_query_by_id(data):
+    schema = to_schema(data)
+    schema = schema_extender.add_query_by_id(schema)
+    return schema
+
+
+def add_query_by_type(data):
+    schema = to_schema(data)
+    return schema
+
+
+def add_input_types_to_create_objects(data):
+    schema = to_schema(data)
+    return schema
+
+
+def add_mutation_for_creating_objects(data):
+    schema = to_schema(data)
+    return schema
+
+
+def to_schema(data):
+    if type(data) == str:
+        schema = schema_extender.read_schema_file(data)
+    elif type(data) == GraphQLSchema:
+        schema = data
     else:
         raise ValueError('Expected input to be a schema or input file')
 
     return schema
-
-
-def add_query_by_type(input):
-    if type(input) == str:
-        schema = schema_extender.read_schema_file(input)
-    elif type(input) == GraphQLSchema:
-        schema = input
-    else:
-        raise ValueError('Expected input to be a schema or input file')
-
-    return schema
-
-
-def add_input_types_to_create_objects(input):
-    if type(input) == str:
-        schema = schema_extender.read_schema_file(input)
-    elif type(input) == GraphQLSchema:
-        schema = input
-    else:
-        raise ValueError('Expected input to be a schema or input file')
-
-    return schema
-
-
-def add_mutation_for_creating_objects(input):
-    if type(input) == str:
-        schema = schema_extender.read_schema_file(input)
-    elif type(input) == GraphQLSchema:
-        schema = input
-    else:
-        raise ValueError('Expected input to be a schema or input file')
-
-    return schema
-
-
-def copy_file(input_file, output_file):
-    with open(input_file, 'r') as file_in, open(output_file, 'w') as file_out:
-        for line in file_in:
-            file_out.write(line)
 
 
 def load_config():
@@ -79,3 +54,7 @@ def print_config(config):
         print('Section: {0}'.format(section));
         for arg in config[section]:
             print('   {0} = {1}'.format(arg, config[section][arg]))
+
+
+def pprint(schema):
+    print(print_schema(schema))

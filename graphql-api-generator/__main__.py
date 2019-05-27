81f07eb9c2d09f9a0d80e81dc1230eb1f50dce60
Robin Keskisärkkä <robin.keskisarkka@liu.se>
2019-05-27 14:09:17 +0200

    use SPIRIT schema as example

--- graphql-api-generator/__main__.py
+++ graphql-api-generator/__main__.py
@@ -4,7 +4,7 @@ import app
 
 if __name__ == '__main__':
     config = app.load_config()
-    schema = app.to_schema('resources/example-schema.graphql')
+    schema = app.to_schema('resources/schema-spirit.graphql')
     schema = app.add_id_to_type(schema)
     schema = app.add_query_by_id(schema)
     schema = app.add_query_by_type(schema)

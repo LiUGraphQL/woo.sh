b8ad5a3af16ff25d1511fc73cd8b634158e5e1a6
Robin Keskisärkkä <robin.keski@gmail.com>
2019-06-28 12:49:16 +0200

    use simple example

--- graphql-api-generator/main.py
+++ graphql-api-generator/main.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 import generator
 
-files = ['../graphql-schema/SpiritContentDB_DB_Schema_DetectionResults.graphql',
-         '../graphql-schema/SpiritContentDB_DB_Schema_Directives.graphql',
-         '../graphql-schema/SpiritContentDB_DB_Schema_Investigations.graphql',
-         '../graphql-schema/SpiritContentDB_DB_Schema_Jobs.graphql',
-         '../graphql-schema/SpiritContentDB_DB_Schema_ScalarTypes.graphql',
-         '../graphql-schema/SpiritContentDB_DB_Schema_SearchResults.graphql',
-         '../graphql-schema/SpiritContentDB_DB_Schema_SocialGraph.graphql']
 files = ['./resources/schema.graphql']
 generator.run(','.join(files), None, True, './resources/config.cfg')
 #generator.run(','.join(files), './resources/api-schema.graphql', True)
